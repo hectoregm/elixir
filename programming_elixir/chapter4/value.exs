7bff92248013ec7851a298c91cda85ad98cec5ed
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-15 00:17:38 -0500

    Finished chapter 4.

--- programming_elixir/chapter4/value.exs
+++ programming_elixir/chapter4/value.exs
@@ -26,9 +26,51 @@ IO.puts foo
 
 # Ranges
 range = 1..10
+IO.inspect range
 
 # regular expressions
 integers = ~r{[0-9]*}
-Regex.run integers, "1234"
+IO.inspect Regex.run integers, "1234"
 
-Regex.run ~r{[aeiou]}, "caterpillar"
+IO.inspect Regex.split ~r{[aeiou]}, "caterpillar"
+
+# PIDS
+IO.inspect self
+
+# Tuples
+tuple_a = { 1, 2 }
+IO.inspect tuple_a
+
+# Lists
+list_a = [1, 2, 3]
+list_b = [4, 5, 6]
+IO.inspect list_a ++ list_b
+IO.inspect list_a -- [2, 4]
+IO.inspect 1 in [1,2,3,4]
+
+# Keyword lists ie list of two-value tuples
+keyword_list = [name: "Dave", city: "Dallas", likes: "Programming"]
+IO.inspect keyword_list
+
+# Maps
+states = %{ "AL"=> "Alabama", "WI" => "Wisconsin" }
+IO.inspect states
+
+responses = %{ {:error, :enoent} => :fatal, {:error, :busy} => :retry }
+IO.inspect responses
+
+# Shortcut when key is an atom
+colors = %{ red: 0xff0000, green: 0x00ff00, blue: 0x0000ff }
+IO.inspect colors
+
+# Binaries
+bin = << 1, 2 >>
+IO.inspect bin
+
+bin = <<3 :: size(2), 5 :: size(4), 1 :: size(2) >>
+:io.format("~-8.2b~n", :binary.bin_to_list(bin))
+IO.inspect byte_size bin
+
+# Boolean operators: or, and, not first argument must be false or true
+
+# Relaxed Boolean operators: ||, &&, !.

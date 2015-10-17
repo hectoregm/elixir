6c79947c274445b4fe4dd99297c32228c81c0eba
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-16 22:12:57 -0500

    First exercises.

--- programming_elixir/chapter5/functions.exs
+++ programming_elixir/chapter5/functions.exs
@@ -1,8 +1,17 @@
-sum = fn (a, b) -> a + b end
-sum.(4, 5)
-
 greet = fn -> IO.puts "Hello" end
 greet.()
 
 f1 = fn a, b -> a * b end
 IO.puts f1.(4, 5)
+
+swap = fn {a, b} -> {b, a} end
+IO.inspect swap.({1, 2})
+
+list_concat= fn a, b -> a ++ b end
+IO.inspect list_concat.([1,2],[3,4])
+
+sum = fn a, b, c -> a + b + c end
+IO.inspect sum.(1,2,3)
+
+pair_tuple_to_list = fn {a, b} -> [a, b] end
+IO.inspect pair_tuple_to_list.({1234, 5678})

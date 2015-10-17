e217c24aba4820c53b834158563224495237a000
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-16 22:18:52 -0500

    pattern matching with functions.

--- programming_elixir/chapter5/functions.exs
+++ programming_elixir/chapter5/functions.exs
@@ -15,3 +15,12 @@ IO.inspect sum.(1,2,3)
 
 pair_tuple_to_list = fn {a, b} -> [a, b] end
 IO.inspect pair_tuple_to_list.({1234, 5678})
+
+handle_open = fn
+  {:ok, file} -> "Read data: #{IO.read(file, :line)}"
+  {_,  error} -> "Error #{:file.format_error(error)}"
+end
+
+
+IO.inspect handle_open.(File.open("foo"))
+IO.inspect handle_open.(File.open("functions.exs"))

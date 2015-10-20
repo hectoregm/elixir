8bdfb67e9756c49695336216033002cefee8b6b9
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-19 23:54:49 -0500

    Finished chapter 5.

--- programming_elixir/chapter5/functions.exs
+++ programming_elixir/chapter5/functions.exs
@@ -38,3 +38,32 @@ end
 
 IO.inspect fizzbuzz.(0, 0, 1)
 IO.inspect [ fb.(10), fb.(11), fb.(12), fb.(13), fb.(14), fb.(15), fb.(16) ]
+
+# Closures
+greeter = fn name -> (fn -> "Hello #{name}" end) end
+hector_greeter = greeter.("Hector")
+IO.inspect hector_greeter.()
+
+prefix = fn prefix_string ->
+  fn string ->
+    "#{prefix_string} #{string}"
+  end
+end
+
+coolcat_prefix = prefix.("coolcat")
+IO.inspect coolcat_prefix.("Hello")
+
+list = [1, 3, 5, 7, 9]
+IO.inspect Enum.map list, fn elem -> elem * 2 end
+
+add_one = &(&1 + 1) # same as add_one = fn (n) -> n + 1 end
+IO.inspect add_one.(9)
+
+divrem = &{ div(&1,&2), rem(&1,&2) }
+IO.inspect divrem.(13,5)
+
+IO.inspect Enum.map [1,2,3,4], &(&1 + 1)
+
+IO.inspect Enum.map [1,2,3,4], &(&1 + 2)
+IO.inspect Enum.each [1,2,3,4], &(IO.inspect &1)
+

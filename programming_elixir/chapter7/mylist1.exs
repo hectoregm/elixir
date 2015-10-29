ec6a384ec79ba7d5f30df1422091f3f06380020d
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-29 10:51:23 -0600

    Using accumulators

--- programming_elixir/chapter7/mylist1.exs
+++ programming_elixir/chapter7/mylist1.exs
@@ -3,4 +3,6 @@ defmodule MyList do
   def len([_head | tail]), do: 1 + len(tail)
   def square([]), do: []
   def square([head | tail]), do: [head*head | square(tail)]
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end

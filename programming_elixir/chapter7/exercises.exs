3b701e3fb23cd399c761a09f7bb1c4873b690adb
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-10-29 19:54:53 -0600

    Exercises from chapter 7.

---
+++ programming_elixir/chapter7/exercises.exs
@@ -0,0 +1,21 @@
+defmodule MyList do
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+  def reduce([], value, _) do
+    value
+  end
+  def reduce([head | tail], value, fun) do
+    reduce(tail, fun.(head, value), fun)
+  end
+  def mapsum([], _), do: []
+  #def mapsum(list, func), do: reduce(map(list, func), 0, &(&1 + &2))
+  def mapsum(list, func), do: map(list, func) |> reduce(0, &(&1 + &2))
+  def max([]), do: 0
+  def max(list), do: reduce(list, 0, fn
+    a, b -> Kernel.max(a, b)
+  end)
+  def caesar(list, n), do: map(list, fn
+    el when el + n <= ?z -> el + n
+    el -> el + n - 26
+  end)
+end

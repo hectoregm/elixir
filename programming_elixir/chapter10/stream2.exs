f4aad246ef5afda58ab7cdaebe1e3b5901a8f5b8
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-11-01 23:24:09 -0600

    File stream.

---
+++ programming_elixir/chapter10/stream2.exs
@@ -0,0 +1,6 @@
+IO.puts File.open!("/usr/share/dict/words")
+        |> IO.stream(:line)
+        |> Enum.max_by(&String.length/1)
+
+# Shortcut
+IO.puts File.stream!("/usr/share/dict/words") |> Enum.max_by(&String.length/1)

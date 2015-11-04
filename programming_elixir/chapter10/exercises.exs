e6a28d0ce2b925a3d560d11f303df1af44a9e430
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-11-03 22:28:43 -0600

    Prime generation with list comprehension

--- programming_elixir/chapter10/exercises.exs
+++ programming_elixir/chapter10/exercises.exs
@@ -6,4 +6,11 @@ defmodule MyList do
   def flatten([head | tail]) do
     [head | flatten(tail)]
   end
+  def span(from, to) when from > to, do: []
+  def span(from, to), do: [from | span(from+1, to)]
+  def primes(n), do: for x <- MyList.span(2,n), MyList.prime?(x), do: x
+  def prime?(n), do: prime?(n, n - 1)
+  defp prime?(_, 1), do: true
+  defp prime?(n, d), do: rem(n,d) != 0 && prime?(n, d - 1)
 end
+IO.inspect MyList.primes(13)

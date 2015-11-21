4bcc906c765f3b3abedb4b9c4fe3392504e6a511
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-11-20 22:57:40 -0600

    Add more tests

--- programming_elixir/issues/test/cli_test.exs
+++ programming_elixir/issues/test/cli_test.exs
@@ -7,4 +7,12 @@ defmodule CliTest do
     assert parse_args(["-h", "anything"]) == :help
     assert parse_args(["--help", "anything"]) == :help
   end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99}
+  end
+
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
 end

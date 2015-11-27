9b17d7213f2e32b717389b734d8b311cd50c84ee
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-11-26 23:09:13 -0600

    Use correct API for httpoison.

--- programming_elixir/issues/lib/issues/github_issues.ex
+++ programming_elixir/issues/lib/issues/github_issues.ex
@@ -10,6 +10,10 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: { :ok, body }
-  def handle_response(%{status_code: _, body: body}), do: { :error, body }
+  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    { :ok, body }
+  end
+  def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    { :error, reason }
+  end
 end

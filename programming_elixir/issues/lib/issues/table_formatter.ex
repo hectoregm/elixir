f025663c89f95b2bb425eb35d7c08e3be6d54fe7
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-11-30 23:10:55 -0600

    Table formatter

---
+++ programming_elixir/issues/lib/issues/table_formatter.ex
@@ -0,0 +1,26 @@
+defmodule Issues.TableFormatter do
+  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
+
+  def print_table_for_columns(rows, headers) do
+    data_by_columns = split_into_columns(rows, headers)
+    columns_widths = widths_of(data_by_columns)
+    format = format_for(columns_widths)
+
+    puts_one_line_in_columns headers, format
+    IO.puts separator(columns_widths)
+    puts_in_columns data_by_columns, format
+  end
+
+  def split_into_columns(rows, headers) do
+    for header <- headers do
+      for row <- rows, do: printable(row[header])
+    end
+  end
+
+  def printable(str) when is_binary(str), do: str
+  def printable(str), do: to_string(str)
+
+  def widths_of(columns) do
+    for column <- columns, do: column |> map(&String.length/1) |> max
+  end
+end

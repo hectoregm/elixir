6c9d74c16298d0c60a8896f64b63bc1e66c17a4a
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-12-03 20:45:30 -0600

    Fix bugs and added one more test

--- programming_elixir/issues/lib/issues/table_formatter.ex
+++ programming_elixir/issues/lib/issues/table_formatter.ex
@@ -25,7 +25,11 @@ defmodule Issues.TableFormatter do
   end
 
   def format_for(column_widths) do
-    map_join(column_withs, "-+-", fn width -> List.duplicate("-", width) end)
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+  end
+
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
   end
 
   def puts_in_columns(data_by_columns, format) do
@@ -35,7 +39,7 @@ defmodule Issues.TableFormatter do
     |> each(&puts_one_line_in_columns(&1, format))
   end
 
-  def puts_one_line_in_columns(field, format) do
+  def puts_one_line_in_columns(fields, format) do
     :io.format(format, fields)
   end
 end

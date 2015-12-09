8cf8f295572a42ea01e7690754dac1ecdd0c0e81
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-12-08 22:26:04 -0600

    Add logging

--- programming_elixir/issues/lib/issues/github_issues.ex
+++ programming_elixir/issues/lib/issues/github_issues.ex
@@ -1,7 +1,10 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
   @user_agent [ { "User-agent", "Elixir hector@hectoregm.com "}]
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,9 +16,12 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     { :ok, :jsx.decode(body) }
   end
   def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    Logger.error "Error #{status} returned"
     { :error, :jsx.decode(reason) }
   end
 end

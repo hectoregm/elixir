8f8b6ceee8c4b1788daec1ef446ccc775f8e84ae
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-12-29 14:04:42 -0800

    Finish exercise 1

---
+++ programming_elixir/chapter14/exercise1.exs
@@ -0,0 +1,28 @@
+defmodule Exercise1 do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad_function(parent) do
+    send parent, "BOOM!"
+    exit(:boom)
+  end
+
+  def receive_messages do
+    receive do
+      message ->
+        IO.puts "Got #{inspect message}"
+    after 500 ->
+      IO.puts "It seems there's nothing more to do."
+      exit :ok
+    end
+    receive_messages
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Exercise1, :sad_function, [self])
+    sleep 500
+    receive_messages
+  end
+end
+
+Exercise1.run

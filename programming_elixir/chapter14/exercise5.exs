659f8d97bc8d8b1cf5c4c9b76e12f40b9fef2564
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-12-30 22:53:17 -0800

    Playing with spawn_monitor.

---
+++ programming_elixir/chapter14/exercise5.exs
@@ -0,0 +1,29 @@
+defmodule Exercise5 do
+  import :timer, only: [ sleep: 1 ]
+
+  def sad_function(parent) do
+    send parent, "BOOM!"
+    #raise "Boom Exception"
+    exit(:boom)
+  end
+
+  def receive_messages do
+    receive do
+      message ->
+        IO.puts "Got #{inspect message}"
+    after 500 ->
+        IO.puts "It seems there's nothing more to do."
+        exit :ok
+    end
+    receive_messages
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_monitor(Exercise5, :sad_function, [self])
+    sleep 500
+    receive_messages
+  end
+end
+
+Exercise5.run

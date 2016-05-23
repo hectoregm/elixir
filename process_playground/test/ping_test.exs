d71636ce1f655f934d9d6d9465c51671515dee3b
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-05-23 08:16:16 -0500

    Finish Ping example.

--- process_playground/test/ping_test.exs
+++ process_playground/test/ping_test.exs
@@ -1,17 +1,3 @@
-defmodule Ping do
-  def start do
-    await
-  end
-
-  def await do
-    receive do
-      {:pong, pid} -> send(pid, {:ping, self})
-    end
-
-    await
-  end
-end
-
 defmodule PingTest do
   use ExUnit.Case
 

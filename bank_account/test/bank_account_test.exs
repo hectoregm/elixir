59e39364718e2ad60f0000b9891e404b1376d8b8
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-05-23 08:23:20 -0500

    Basic Bank Account

---
+++ bank_account/test/bank_account_test.exs
@@ -0,0 +1,31 @@
+defmodule BankAccount do
+  def start do
+    await
+  end
+
+  def await do
+    receive do
+      {:check_balance, pid} -> divulge_balance(pid)
+    end
+    await
+  end
+
+  def divulge_balance(pid) do
+    send pid, {:balance, 0}
+  end
+end
+
+defmodule BankAccountTest do
+  use ExUnit.Case
+  doctest BankAccount
+
+  test "starts off with a balance of 0" do
+    account = spawn_link(BankAccount, :start, [])
+    verify_balance_is 0, account
+  end
+
+  def verify_balance_is(expected_balance, account) do
+    send account, {:check_balance, self}
+    assert_receive {:balance, ^expected_balance}
+  end
+end

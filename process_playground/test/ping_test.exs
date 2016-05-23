5bdd07e0514796e5252d6c54578f33eba0a5ca24
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-05-23 07:25:07 -0500

    Playing with receive

--- process_playground/test/ping_test.exs
+++ process_playground/test/ping_test.exs
@@ -1,8 +1,31 @@
+defmodule Ping do
+  def start do
+    await
+  end
+
+  def await do
+    receive do
+      {:pong, pid} -> send(pid, {:ping, self})
+    end
+
+    await
+  end
+end
+
 defmodule PingTest do
   use ExUnit.Case
 
   test "it responds to a pong with a ping" do
     ping = spawn_link(Ping, :start, [])
+    Process.send ping, {:pong, self}, []
+    assert_receive {:ping, ^ping}
+  end
+
+  test "it responds to two pongs with two pings" do
+    ping = spawn_link(Ping, :start, [])
+    Process.send ping, {:pong, self}, []
+    assert_receive {:ping, ^ping}
+
     Process.send ping, {:pong, self}, []
     assert_receive {:ping, ^ping}
   end
